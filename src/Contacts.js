d5aa56e065c4eed621ec253f1b175c07e2fe7cd2
austint33 <austintumlinson@gmail.com>
2020-06-25 17:01:07 -0500

    first steps for stateless components

---
+++ src/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <ul>
+            <li>Name one</li>
+            <li>Name two</li>
+        </ul>
+    )
+}
+
+export default Contacts;
